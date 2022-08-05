ff02e54c55fc495f5a2000aca03665b31d2d2e4d
lucas-ifsp <lucas.oliveira@ifsp.edu.br>
2022-08-05 18:12:05 -0300

    refactor: remove commented method and add eslint comment to shutdown warning

--- src/components/game/mat/Mat.jsx
+++ src/components/game/mat/Mat.jsx
@@ -4,14 +4,11 @@ import OpenCards from '../cards/OpenCards'
 import OpponentHand from '../cards/OpponentHand'
 import PlayerHand from '../cards/PlayerHand'
 import Commands from '../commands/Commands'
+import './Mat.css'
 import Message from './Message'
 import { createMessage } from './MessageFactory'
 import Rounds from './Rounds'
 import Score from './Score'
-
-import './Mat.css'
-
-//SOLVE OPPONENT CARD NOT THROWING BUG
 
 const Mat = ({ initialIntel, uuid, token }) => {
     const nextScoreAsString = {1: 'truco', 3: 'seis', 6: 'nove', 9: 'doze', 12: 'doze'}
@@ -53,6 +50,7 @@ const Mat = ({ initialIntel, uuid, token }) => {
     useEffect(() => {
         if(!missingIntel || missingIntel.length === 0) return
         animate()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [missingIntel])
 
     async function animate(){
@@ -60,7 +58,6 @@ const Mat = ({ initialIntel, uuid, token }) => {
             setMissingIntel([])
             return
         }
-    
         const prevIntel = missingIntel[0]
         const currentIntel = missingIntel[1]
 
@@ -94,7 +91,6 @@ const Mat = ({ initialIntel, uuid, token }) => {
                 await delay(DELAY_UNIT * 30)
                 clearOpenCards()
             } 
-        
             await delay(DELAY_UNIT * 3)
             updateButtons(currentIntel)
             await delay(DELAY_UNIT * 2)
@@ -103,64 +99,6 @@ const Mat = ({ initialIntel, uuid, token }) => {
         const remainingIntel = missingIntel.slice(1, missingIntel.length)
         setMissingIntel(remainingIntel)
     }
-
-    // async function animate(){
-    //     const DELAY_UNIT = 120;
-    //     currentIntel = missingIntel[0];
-
-    //     if(currentIntel.event === 'HAND_START'){
-    //         prepareNewHand(currentIntel, DELAY_UNIT * 15)
-    //     } else if(currentIntel.event === 'GAME_OVER'){
-    //         renderLater(DELAY_UNIT, getPlayer(currentIntel).score, playerScore, setPlayerScore)
-    //         renderLater(0, getOpponent(currentIntel).score, opponentScore, setOpponentScore)
-    //     } else {
-    //         renderLater(DELAY_UNIT * 15, currentIntel.handPoints, handPoints, setHandPoints)
-    //         renderLater(DELAY_UNIT * 5, getUpdatedHand(getOpponent(currentIntel).cards, opponentHand), opponentHand, setOpponentHand)
-    //         renderLater(DELAY_UNIT * 3, getUpdatedHand(getPlayer(currentIntel).cards, playerHand), playerHand, setPlayerHand)
-            
-    //         if(hasChangedMatchProperty('openCards', currentIntel, prevIntel)) {
-    //             await delay(DELAY_UNIT * 3)
-    //             updateOpenCards(currentIntel)
-    //         }
-
-    //         renderLater(DELAY_UNIT * 3, currentIntel.roundWinnersUsernames, rounds, setRounds)
-    //         renderLater(DELAY_UNIT * 30, null, playerCard, setPlayerCard)
-    //         renderLater(0, null, opponentCard, setOpponentCard)
-        
-    //         await delay()
-    //         updateButtons(DELAY_UNIT * 3, currentIntel)
-    //         await delay(DELAY_UNIT * 2)
-    //         updateMessage(currentIntel)
-
-    //         const remainingIntel = missingIntel.slice(1, missingIntel.length)
-    //         setMissingIntel(remainingIntel)
-    //     }
-    // }
-
-    // async function renderLater(newState, state, setState, millis){
-    //     if(JSON.stringify(newState) !== JSON.stringify(state)){
-    //         await delay(millis)
-    //         setState(newState)
-    //     }
-    // }
-
-    // function prepareNewHand(intel, millis){
-    //     renderLater(millis, 1, handPoints, setHandPoints, millis)
-    //     renderLater(0, getPlayer(intel).score, playerScore, setPlayerScore())
-    //     renderLater(0, getOpponent(intel).score, opponentScore, setOpponentScore)
-    //     renderLater(0, toCardString(intel.vira), vira, setVira)
-
-    //     console.log('------- ' + new Date().getTime())
-    //     console.log('New hand')
-    //     console.log(intel)
-
-    //     renderLater(0, getCardsAsStrings(getPlayer(intel).cards), playerHand, setPlayerHand)
-    //     renderLater(0, getCardsAsStrings(getOpponent(intel).cards), opponentHand, setOpponentHand)
-    //     renderLater(0, intel.roundWinnersUsernames, rounds, setRounds)
-
-    //     renderLater(0, null, playerCard, setPlayerCard)
-    //     renderLater(0, null, opponentCard, setOpponentCard)
-    // }
 
     function prepareNewHand(intel){
         setHandPoints(1)
@@ -189,14 +127,6 @@ const Mat = ({ initialIntel, uuid, token }) => {
         setPlayerCard(null)
         setOpponentCard(null)
     }
-
-    // function getUpdatedHand(cards, cardState) {
-    //     const getSameFromIntelOrNull = (handFromIntel, someCard) => handFromIntel.find(card => card === someCard) || null
-    //     const getUpdated = (receivedHand, handFromIntel) => receivedHand.map(card => getSameFromIntelOrNull(handFromIntel, card))
-    //     const cardsFromIntel = getCardsAsStrings(cards)
-    //     const updateHand = getUpdated(cardState, cardsFromIntel)
-    //     return updateHand
-    // }
 
     function updateHand(cards, cardState, setCardStateFunction) {
         const getSameFromIntelOrNull = (handFromIntel, someCard) => handFromIntel.find(card => card === someCard) || null
