c037a273ce58d00ec420e92ca9f294527bc90c21
lucas-ifsp <lucas.oliveira@ifsp.edu.br>
2022-08-05 17:20:28 -0300

    fix: solves the opponent bug disapearing

--- src/components/game/mat/Mat copy.jsx
+++ src/components/game/mat/Mat copy.jsx
@@ -14,6 +14,7 @@ import './Mat.css'
 //SOLVE OPPONENT CARD NOT THROWING BUG
 
 const Mat = ({ initialIntel, uuid, token }) => {
+    const endpoint = 'http://localhost:8080'
     const nextScoreAsString = {1: 'truco', 3: 'seis', 6: 'nove', 9: 'doze', 12: 'doze'}
 
     const toCardString = card => card.rank === 'X' ? 'back' : `${card.rank}${card.suit}`
@@ -21,10 +22,10 @@ const Mat = ({ initialIntel, uuid, token }) => {
     const getPlayer = intel => intel.players.find(aPlayer => aPlayer.uuid === uuid)
     const getOpponent = intel => intel.players.find(aPlayer => aPlayer.uuid !== uuid)
     const canPerform = (intel, action) =>  intel.currentPlayerUuid === uuid && intel.possibleActions.includes(action)
-    const delay = ms => new Promise(res => setTimeout(res, ms))
 
     let player = getPlayer(initialIntel)
     let opponent = getOpponent(initialIntel)
+
 
     const [lastIntel, setLastIntel] = useState(initialIntel)
     const [missingIntel, setMissingIntel] = useState([initialIntel])
@@ -49,129 +50,109 @@ const Mat = ({ initialIntel, uuid, token }) => {
     const [raiseLabel, setRaiseLabel] = useState('Pedir truco')
     const [quitLabel, setQuitLabel] = useState('Correr')
 
+    //TESTAR SE ESTÁ PARTICIONANDO CERTINHO
+    //TESTAR O QUE DIÁBOS ESTÁ ACONTECENDO COM A PORRA DA MÃO DO USUÁRIO.
     useEffect(() => {
-        if(!missingIntel || missingIntel.length === 0) return
-        //console.log('-------')
-        //console.log('Animating single hand')
-        animate()
-    }, [missingIntel])
+        if(!missingIntel) return
 
-    useEffect(() => {
-        console.log('Updated opponent hand use effect: ' + new Date().getTime())
-        console.log(opponentHand)
-    }, [opponentHand])
+        const secondHandIndex = missingIntel.findIndex(intel => intel.event === 'HAND_START')
+        if(secondHandIndex != -1){
+            const firstHandIntel = missingIntel.slice(0, secondHandIndex)
+            const secondHandIntel = missingIntel.slice(secondHandIndex, missingIntel.length)
+            animateIntel(firstHandIntel)
+            setMissingIntel(...secondHandIntel)
+            setLastIntel(secondHandIntel.splice(-1)[0])
+        }
+        animateIntel(missingIntel)
+        setMissingIntel([])
+    }, [lastIntel])
 
-
-    async function animate(){
-       // const delay = ms => new Promise(res => setTimeout(res, ms))
+    async function animateIntel(missingIntel){
+        const delay = ms => new Promise(res => setTimeout(res, ms))
         const DELAY_UNIT = 120;
 
-        if(missingIntel.length === 1){
-            setMissingIntel([])
-            return
-        }
-    
-        const prevIntel = missingIntel[0]
-        const currentIntel = missingIntel[1]
+        for(let i = 1; i < missingIntel.length; i++) {
+            const prevIntel = missingIntel[i - 1]
+            const currentIntel = missingIntel[i]
 
-        if(currentIntel.event === 'HAND_START'){
-            await delay(DELAY_UNIT * 15)
-            prepareNewHand(currentIntel)
-        } else if(currentIntel.event === 'GAME_OVER'){
-            await delay(DELAY_UNIT)
-            setPlayerScore(getPlayer(currentIntel).score)
-            setOpponentScore(getOpponent(currentIntel).score)
-        } else {
-            if(hasChangedMatchProperty('handPoints', currentIntel, prevIntel)) {
+            if(currentIntel.event === 'HAND_START'){
                 await delay(DELAY_UNIT * 15)
-                setHandPoints(currentIntel.handPoints)
+                prepareNewHand(currentIntel)
+            } else if(currentIntel.event === 'GAME_OVER'){
+                await delay(DELAY_UNIT)
+                setPlayerScore(getPlayer(currentIntel).score)
+                setOpponentScore(getOpponent(currentIntel).score)
+            } else {
+                if(hasChangedMatchProperty('handPoints', currentIntel, prevIntel)) {
+                    await delay(DELAY_UNIT * 15)
+                    setHandPoints(currentIntel.handPoints)
+                }
+                if(hasChangedOpponentProperty('cards', currentIntel, prevIntel)) {
+                    await delay(DELAY_UNIT * 5)
+                    updateHand(getOpponent(currentIntel).cards, opponentHand, setOpponentHand)
+                }
+                if(hasChangedPlayerProperty('cards', currentIntel, prevIntel)) {
+                    await delay(DELAY_UNIT * 3)
+                    updateHand(getPlayer(currentIntel).cards, playerHand, setPlayerHand)
+                }
+                if(hasChangedMatchProperty('openCards', currentIntel, prevIntel)) {
+                    await delay(DELAY_UNIT * 3)
+                    updateOpenCards(currentIntel)
+                }
+                if(hasChangedMatchProperty('roundWinnersUsernames', currentIntel, prevIntel)){
+                    await delay(DELAY_UNIT * 3)
+                    setRounds(currentIntel.roundWinnersUsernames)
+                    await delay(DELAY_UNIT * 30)
+                    clearOpenCards()
+                } 
             }
-            if(hasChangedOpponentProperty('cards', currentIntel, prevIntel)) {
-                await delay(DELAY_UNIT * 5)
-                console.log('Opponent hand state before animate ' +  new Date().getTime())
-                console.log(opponentHand)
-                updateHand(getOpponent(currentIntel).cards, opponentHand, setOpponentHand, true) // remove boolean after finding the bug
-            }
-            if(hasChangedPlayerProperty('cards', currentIntel, prevIntel)) {
-                await delay(DELAY_UNIT * 3)
-                updateHand(getPlayer(currentIntel).cards, playerHand, setPlayerHand, false)  // remove boolean after finding the bug
-            }
-            if(hasChangedMatchProperty('openCards', currentIntel, prevIntel)) {
-                await delay(DELAY_UNIT * 3)
-                updateOpenCards(currentIntel)
-            }
-            if(hasChangedMatchProperty('roundWinnersUsernames', currentIntel, prevIntel)){
-                await delay(DELAY_UNIT * 3)
-                setRounds(currentIntel.roundWinnersUsernames)
-                await delay(DELAY_UNIT * 30)
-                clearOpenCards()
-            } 
-        
-            await delay(DELAY_UNIT * 3)
             updateButtons(currentIntel)
-            await delay(DELAY_UNIT * 2)
             updateMessage(currentIntel)
         }
-        const remainingIntel = missingIntel.slice(1, missingIntel.length)
-        setMissingIntel(remainingIntel)
     }
 
-    // async function animate(){
+    // useEffect(() => {
+    //     if(!missingIntel) return
+    //     const timeline = buildTimeline(missingIntel)
+    //     timeline.forEach(animation => setTimeout(() => animation.event(), animation.time))
+    //     setMissingIntel([])
+    // }, [lastIntel])
+
+    // function buildTimeline(missingIntel){
     //     const DELAY_UNIT = 120;
-    //     currentIntel = missingIntel[0];
+    //     const timeline = new Timeline()
 
-    //     if(currentIntel.event === 'HAND_START'){
-    //         prepareNewHand(currentIntel, DELAY_UNIT * 15)
-    //     } else if(currentIntel.event === 'GAME_OVER'){
-    //         renderLater(DELAY_UNIT, getPlayer(currentIntel).score, playerScore, setPlayerScore)
-    //         renderLater(0, getOpponent(currentIntel).score, opponentScore, setOpponentScore)
-    //     } else {
-    //         renderLater(DELAY_UNIT * 15, currentIntel.handPoints, handPoints, setHandPoints)
-    //         renderLater(DELAY_UNIT * 5, getUpdatedHand(getOpponent(currentIntel).cards, opponentHand), opponentHand, setOpponentHand)
-    //         renderLater(DELAY_UNIT * 3, getUpdatedHand(getPlayer(currentIntel).cards, playerHand), playerHand, setPlayerHand)
-            
-    //         if(hasChangedMatchProperty('openCards', currentIntel, prevIntel)) {
-    //             await delay(DELAY_UNIT * 3)
-    //             updateOpenCards(currentIntel)
+    //     for(let i = 1; i < missingIntel.length; i++) {
+    //         const prevIntel = missingIntel[i - 1]
+    //         const currentIntel = missingIntel[i]
+
+    //         if(currentIntel.event === 'HAND_START'){
+    //             timeline.addEvent(DELAY_UNIT * 15, async () => prepareNewHand(currentIntel))
+    //         } else if(currentIntel.event === 'GAME_OVER'){
+    //             timeline.addEvent(DELAY_UNIT, async () => setPlayerScore(getPlayer(currentIntel).score))
+    //             timeline.addEvent(0, async () => setOpponentScore(getOpponent(currentIntel).score))
+    //         } else {
+    //             if(hasChangedMatchProperty('handPoints', currentIntel, prevIntel)) 
+    //                 timeline.addEvent(DELAY_UNIT * 3, async () => setHandPoints(currentIntel.handPoints))
+
+    //             if(hasChangedOpponentProperty('cards', currentIntel, prevIntel)) 
+    //                 timeline.addEvent(DELAY_UNIT * 5, async () => updateHand(getOpponent(currentIntel).cards, opponentHand, setOpponentHand))
+                
+    //             if(hasChangedPlayerProperty('cards', currentIntel, prevIntel)) 
+    //                 timeline.addEvent(DELAY_UNIT * 3, async () => updateHand(getPlayer(currentIntel).cards, playerHand, setPlayerHand))
+
+    //             if(hasChangedMatchProperty('openCards', currentIntel, prevIntel)) 
+    //                 timeline.addEvent(DELAY_UNIT * 3, async () => updateOpenCards(currentIntel))
+
+    //             if(hasChangedMatchProperty('roundWinnersUsernames', currentIntel, prevIntel)){
+    //                 timeline.addEvent(DELAY_UNIT * 3, async () => setRounds(currentIntel.roundWinnersUsernames))
+    //                 timeline.addEvent(DELAY_UNIT * 30, async () => clearOpenCards())
+    //             } 
     //         }
-
-    //         renderLater(DELAY_UNIT * 3, currentIntel.roundWinnersUsernames, rounds, setRounds)
-    //         renderLater(DELAY_UNIT * 30, null, playerCard, setPlayerCard)
-    //         renderLater(0, null, opponentCard, setOpponentCard)
-        
-    //         await delay()
-    //         updateButtons(DELAY_UNIT * 3, currentIntel)
-    //         await delay(DELAY_UNIT * 2)
-    //         updateMessage(currentIntel)
-
-    //         const remainingIntel = missingIntel.slice(1, missingIntel.length)
-    //         setMissingIntel(remainingIntel)
+    //         timeline.addEvent(DELAY_UNIT * 3, async () => updateButtons(currentIntel))  
+    //         timeline.addEvent(DELAY_UNIT * 2, async () => updateMessage(currentIntel))  
     //     }
-    // }
-
-    // async function renderLater(newState, state, setState, millis){
-    //     if(JSON.stringify(newState) !== JSON.stringify(state)){
-    //         await delay(millis)
-    //         setState(newState)
-    //     }
-    // }
-
-    // function prepareNewHand(intel, millis){
-    //     renderLater(millis, 1, handPoints, setHandPoints, millis)
-    //     renderLater(0, getPlayer(intel).score, playerScore, setPlayerScore())
-    //     renderLater(0, getOpponent(intel).score, opponentScore, setOpponentScore)
-    //     renderLater(0, toCardString(intel.vira), vira, setVira)
-
-    //     console.log('------- ' + new Date().getTime())
-    //     console.log('New hand')
-    //     console.log(intel)
-
-    //     renderLater(0, getCardsAsStrings(getPlayer(intel).cards), playerHand, setPlayerHand)
-    //     renderLater(0, getCardsAsStrings(getOpponent(intel).cards), opponentHand, setOpponentHand)
-    //     renderLater(0, intel.roundWinnersUsernames, rounds, setRounds)
-
-    //     renderLater(0, null, playerCard, setPlayerCard)
-    //     renderLater(0, null, opponentCard, setOpponentCard)
+    //     return timeline.get()  
     // }
 
     function prepareNewHand(intel){
@@ -179,9 +160,6 @@ const Mat = ({ initialIntel, uuid, token }) => {
         setPlayerScore(getPlayer(intel).score)
         setOpponentScore(getOpponent(intel).score)
         setVira(toCardString(intel.vira))
-        console.log('------- ' + new Date().getTime())
-        console.log('New hand')
-        console.log(intel)
         setPlayerHand(getCardsAsStrings(getPlayer(intel).cards))
         setOpponentHand(getCardsAsStrings(getOpponent(intel).cards))
         setRounds(intel.roundWinnersUsernames)
@@ -205,24 +183,11 @@ const Mat = ({ initialIntel, uuid, token }) => {
         setOpponentCard(null)
     }
 
-    // function getUpdatedHand(cards, cardState) {
-    //     const getSameFromIntelOrNull = (handFromIntel, someCard) => handFromIntel.find(card => card === someCard) || null
-    //     const getUpdated = (receivedHand, handFromIntel) => receivedHand.map(card => getSameFromIntelOrNull(handFromIntel, card))
-    //     const cardsFromIntel = getCardsAsStrings(cards)
-    //     const updateHand = getUpdated(cardState, cardsFromIntel)
-    //     return updateHand
-    // }
-
-    function updateHand(cards, cardState, setCardStateFunction, bot) {
+    function updateHand(cards, cardState, setCardStateFunction) {
         const getSameFromIntelOrNull = (handFromIntel, someCard) => handFromIntel.find(card => card === someCard) || null
         const getUpdatedHand = (receivedHand, handFromIntel) => receivedHand.map(card => getSameFromIntelOrNull(handFromIntel, card))
         const cardsFromIntel = getCardsAsStrings(cards)
         const updateHand = getUpdatedHand(cardState, cardsFromIntel)
-        
-        if(bot) {
-            console.log('------- ' +  new Date().getTime())
-            console.log(`Cards before: ${cardState} | Intel cards: ${cardsFromIntel} | Cards after: ${updateHand} `)
-        }
         setCardStateFunction(updateHand)
     }
 
