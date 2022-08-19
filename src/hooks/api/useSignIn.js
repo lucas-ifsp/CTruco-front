371835e247e72114d5dc5d7d2c6c00fd31f93de0
lucas-ifsp <lucas.oliveira@ifsp.edu.br>
2022-08-19 12:28:03 -0300

    refactor: use custom hook to encapsulate singIn api request

---
+++ src/hooks/api/useSignIn.js
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import axios from '../../api/axios'
+import useAuth from '../context/useAuth'
+
+const useSignIn = () => {
+    const [success, setSuccess] = useState(false)
+    const [error, setError] = useState()
+    const { setAuth } = useAuth()
+
+    const signIn = async (payload) => {
+        try {
+            setError(false)
+            const { headers: { authorization: token }, data: { refreshToken, uuid } } = await axios.post(`/login`, payload)
+            setAuth({ token, uuid, refreshToken, username: payload.username })
+            setSuccess(true)
+        } catch (error) {
+            console.log(error.message)
+            if (!error.response) setError('Sistema temporariamente indisponível.')
+            else if (error.response.status === 401) setError('Usuário ou senha inválidos.')
+            else setError('Algo deu errado.')
+        }
+    }
+    return [signIn, success, error]
+}
+
+export default useSignIn
--- src/pages/authentication/useAuthenticationForm.js
+++ src/pages/authentication/useAuthenticationForm.js
@@ -1,7 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { authenticate } from '../../api/AuthenticationApi';
-import useAuth from '../../hooks/context/useAuth';
+import useSignIn from '../../hooks/api/useSignIn';
 
 const useAuthenticationForm = (validate) => {
     const navigate = useNavigate();
@@ -10,8 +9,16 @@ const useAuthenticationForm = (validate) => {
         password: ''
     })
     const [errors, setErrors] = useState({})
-    const { setAuth } = useAuth()
+    const [signIn, success, apiError] = useSignIn()
 
+    useEffect(() => {
+        setErrors(prevState => ({ ...prevState, apiError }))
+    }, [apiError])
+
+    useEffect(() => {
+        if (!success) return
+        navigate('/')
+    }, [success, navigate])
 
     const handleChange = e => {
         const { name, value } = e.target;
@@ -28,15 +35,7 @@ const useAuthenticationForm = (validate) => {
 
         if (hasInputErrors(valitationErrors)) return
 
-        try {
-            const requestPayload = { username: values.username, password: values.password }
-            const response = await authenticate(requestPayload)
-            setAuth({ ...response, username: values.username })
-            navigate('/')
-        } catch (error) {
-            const requestError = { apiError: error.message }
-            setErrors(() => requestError)
-        }
+        signIn({ username: values.username, password: values.password })
     }
 
     const hasInputErrors = valitationErrors => Object.keys(valitationErrors).length !== 0
