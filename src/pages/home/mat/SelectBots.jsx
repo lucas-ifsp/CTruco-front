a57b666d983e893e84ba369f25a49dfc4ca13331
gcontiero11 <gustcontiero@gmail.com>
2024-07-11 00:13:40 -0300

    feat: create useGetBotNames to fetch all bot names

--- src/pages/home/mat/SelectBots.jsx
+++ src/pages/home/mat/SelectBots.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import { useState } from "react";
+import useGetBotNames from "../../../hooks/api/useGetBotNames";
 
-const SelectBots = ({botName ,setBotName}) => {
-  const bots = ["MineiroByBueno", "DummyBot", "LazyBot", "VapoBot"];
+const SelectBots = ({ botName, setBotName }) => {
+  const [botsList, setBotsList] = useState([
+    "MineiroByBueno",
+    "DummyBot",
+    "LazyBot",
+    "VapoBot",
+  ]);
+  const fetchBotNames = useGetBotNames();
+  const listBots = () => {
+    fetchBotNames().then((response) => {
+      setBotsList(response.data.sort());
+    });
+  };
+  listBots();
   return (
     <select
       className="form-select mb-3"
       value={botName}
       onChange={(e) => setBotName(e.target.value)}
     >
-      {bots.map((optionBotName) => (
-        <option value={optionBotName} >{optionBotName}</option>
+      {botsList.map((optionBotName) => (
+        <option value={optionBotName} key={optionBotName}>
+          {optionBotName}
+        </option>
       ))}
     </select>
   );
