6af4f12582846086bde63c6e06f6b01ae232e8d1
gcontiero11 <gustcontiero@gmail.com>
2024-07-29 17:19:59 -0300

    feat: starting to create the remoteBot CRUD

--- src/pages/home/crud-remote-bots/AddRemote.jsx
+++ src/pages/home/crud-remote-bots/AddRemote.jsx
@@ -2,15 +2,56 @@ import React from "react";
 import Header from "../../../components/templates/Header";
 import Menu from "../../../components/templates/Menu";
 import Footer from "../../../components/templates/Footer";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Button,
+  Input,
+  ChakraProvider,
+} from "@chakra-ui/react";
 import "./AddRemote.css";
-import "../Home.css"
+import "../Home.css";
 
 const AddRemote = () => {
   return (
     <div className="app">
       <Header />
       <Menu />
-      <p className="cs-feat">CRUD Remote - Comming Soon</p>
+      <form>
+        <ChakraProvider>
+          <label htmlFor="bots_filter"> BUSCAR PELO NOME </label>
+          <br/>
+          <Input type="text" id="bots_filter" />
+          <TableContainer className="bots-table">
+            <Table variant="simple" colorScheme="gray">
+              <Thead>
+                <Tr>
+                  <Th>Bot</Th>
+                  <Th>Endereço</Th>
+                  <Th>Porta</Th>
+                  <Th>Opções</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>RemoteBot</Td>
+                  <Td>http://localhost</Td>
+                  <Td>8080</Td>
+                  <Td>
+                    <Button color={"yellow"}>Editar</Button>
+                    <Button color={"red"}>Remover</Button>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </ChakraProvider>
+      </form>
       <Footer />
     </div>
   );
