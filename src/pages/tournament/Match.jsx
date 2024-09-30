cd252cfaa3dc4fe8fe5c3c9925e16b127338f974
gcontiero11 <gustcontiero@gmail.com>
2024-09-29 21:22:40 -0300

    refactoring

--- src/pages/tournament/Match.jsx
+++ src/pages/tournament/Match.jsx
@@ -1,57 +1,52 @@
 import React from "react";
 
 const Match = ({
-  p1Name,
-  p2Name,
-  p1Score,
-  p2Score,
-  matchNumber,
+  match,
   onPlay,
-  disabled,
 }) => {
   return (
     <>
-      <div className={"match-player m" + matchNumber + " p1"}>
-        <p className="p1">{p1Name || "?"}</p>
-        <div className={"t-score m" + matchNumber}>
+      <div className={"match-player m" + match.matchNumber + " p1"}>
+        <p className="p1">{match.p1Name || "?"}</p>
+        <div className={"t-score m" + match.matchNumber}>
           <p
             className={
-              p1Score === 0 && p2Score === 0
+              match.p1Score === 0 && match.p2Score === 0
                 ? ""
-                : p1Score > p2Score
+                : match.p1Score > match.p2Score
                 ? "winner"
                 : "loser"
             }
           >
-            {p1Score === 0 ? "" : p1Score}
+            {match.p1Score === 0 ? "" : match.p1Score}
           </p>
         </div>
       </div>
-      <div className={"match-button m" + matchNumber + " b"}>
+      <div className={"match-button m" + match.matchNumber + " b"}>
         <button
           className="btn btn-success"
-          onClick={() => onPlay(matchNumber)}
-          disabled={disabled}
+          onClick={() => onPlay(match.matchNumber)}
+          disabled={!(match.p1Name && match.p2Name && !match.winnerName)}
         >
           Jogar
         </button>
       </div>
 
-      <div className={"match-player m" + matchNumber + " p2"}>
-        <div className={"t-score m" + matchNumber}>
+      <div className={"match-player m" + match.matchNumber + " p2"}>
+        <div className={"t-score m" + match.matchNumber}>
           <p
             className={
-              p1Score === 0 && p2Score === 0
+              match.p1Score === 0 && match.p2Score === 0
                 ? ""
-                : p2Score > p1Score
+                : match.p2Score > match.p1Score
                 ? "winner"
                 : "loser"
             }
           >
-            {p2Score === 0 ? "" : p2Score}
+            {match.p2Score === 0 ? "" : match.p2Score}
           </p>
         </div>
-        <p className="p2">{p2Name || "?"}</p>
+        <p className="p2">{match.p2Name || "?"}</p>
       </div>
     </>
   );
--- src/pages/tournament/Tournament.jsx
+++ src/pages/tournament/Tournament.jsx
@@ -38,83 +38,11 @@ const Tournament = () => {
           <div class="alert alert-info info-simulacoes" role="alert">
             Simulações por Partida: {championship.times}
           </div>
-          <Match
-            p1Name={championship.matchesDTO[0]?.p1Name}
-            p2Name={championship.matchesDTO[0]?.p2Name}
-            p1Score={championship.matchesDTO[0]?.p1Score}
-            p2Score={championship.matchesDTO[0]?.p2Score}
-            matchNumber={1}
-            onPlay={playCampMatch}
-            disabled={championship.matchesDTO[0].winnerName}
-          />
-          <Match
-            p1Name={championship.matchesDTO[1]?.p1Name}
-            p2Name={championship.matchesDTO[1]?.p2Name}
-            p1Score={championship.matchesDTO[1]?.p1Score}
-            p2Score={championship.matchesDTO[1]?.p2Score}
-            matchNumber={2}
-            onPlay={playCampMatch}
-            disabled={championship.matchesDTO[1].winnerName}
-          />
-          <Match
-            p1Name={championship.matchesDTO[2]?.p1Name}
-            p2Name={championship.matchesDTO[2]?.p2Name}
-            p1Score={championship.matchesDTO[2]?.p1Score}
-            p2Score={championship.matchesDTO[2]?.p2Score}
-            matchNumber={3}
-            onPlay={playCampMatch}
-            disabled={championship.matchesDTO[2].winnerName}
-          />
-          <Match
-            p1Name={championship.matchesDTO[3]?.p1Name}
-            p2Name={championship.matchesDTO[3]?.p2Name}
-            p1Score={championship.matchesDTO[3]?.p1Score}
-            p2Score={championship.matchesDTO[3]?.p2Score}
-            matchNumber={4}
-            onPlay={playCampMatch}
-            disabled={championship.matchesDTO[3].winnerName}
-          />
 
-          {/* Semifinais e finais */}
-          <Match
-            p1Name={championship.matchesDTO[0]?.winnerName || "?"}
-            p2Name={championship.matchesDTO[1]?.winnerName || "?"}
-            p1Score={championship.matchesDTO[4]?.p1Score}
-            p2Score={championship.matchesDTO[4]?.p2Score}
-            matchNumber={5}
-            onPlay={playCampMatch}
-            disabled={
-              !championship.matchesDTO[0]?.winnerName ||
-              !championship.matchesDTO[1]?.winnerName ||
-              championship.matchesDTO[4].winnerName
-            }
-          />
-          <Match
-            p1Name={championship.matchesDTO[2]?.winnerName || "?"}
-            p2Name={championship.matchesDTO[3]?.winnerName || "?"}
-            p1Score={championship.matchesDTO[5]?.p1Score}
-            p2Score={championship.matchesDTO[5]?.p2Score}
-            matchNumber={6}
-            onPlay={playCampMatch}
-            disabled={
-              !championship.matchesDTO[2]?.winnerName ||
-              !championship.matchesDTO[3]?.winnerName ||
-              championship.matchesDTO[5].winnerName
-            }
-          />
-          <Match
-            p1Name={championship.matchesDTO[4]?.winnerName || "?"}
-            p2Name={championship.matchesDTO[5]?.winnerName || "?"}
-            p1Score={championship.matchesDTO[6]?.p1Score}
-            p2Score={championship.matchesDTO[6]?.p2Score}
-            matchNumber={7}
-            onPlay={playCampMatch}
-            disabled={
-              !championship.matchesDTO[4]?.winnerName ||
-              !championship.matchesDTO[5]?.winnerName ||
-              championship.matchesDTO[6].winnerName
-            }
-          />
+          {championship.matchesDTO.map((match) => (
+            <Match match={match} onPlay={playCampMatch} />
+          ))}
+          
           <div className="match-player winner">
             <p>{championship.winnerName || "?"}</p>
           </div>
